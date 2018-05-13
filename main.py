07cdb5e7604d9f10b198593a2c316226fa95da8f
Jiatu Liu <jiatu@opendoor.com>
2018-05-13 14:52:06 -0700

    Train val test split

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+from torchvision import transforms
+
 from dataset import IMDbFacialDataset
-import matplotlib.pyplot as plt
+
+BATCH_SIZE = 500
 
 
 def main():
-    dataset = IMDbFacialDataset('imdb_crop')
-    print(0, dataset[0].shape)
-    print(460722, dataset[460722].shape)
-    print(40302, dataset[40302].shape)
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
+    dataset = IMDbFacialDataset('imdb_crop', transform)
+    # Do a rough 8:1:1 split between training set, validation set and test set.
+    num_train = int(len(dataset) * 0.8)
+    num_val = int(len(dataset) * 0.1)
+    loader_train = DataLoader(
+        dataset,
+        batch_size=BATCH_SIZE,
+        batch_sampler=sampler.SubsetRandomSampler(range(num_train))
+    )
+    loader_val = DataLoader(
+        dataset,
+        batch_size=BATCH_SIZE,
+        batch_sampler=sampler.SubsetRandomSampler(range(num_train, num_train + num_val))
+    )
+    loader_test = DataLoader(
+        dataset,
+        batch_size=BATCH_SIZE,
+        batch_sampler=sampler.SubsetRandomSampler(range(num_train + num_val, len(dataset)))
+    )
+
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
 
 
 if __name__ == '__main__':
