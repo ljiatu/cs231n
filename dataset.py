Fix loss calculation. Use age buckets instead of concrete age

dataset.py
@@ -4,6 +4,10 @@ import os
 from skimage import io
 from torch.utils.data import Dataset
 
+# Divide ages into 21 buckets. Each bucket contains 5 ages
+# (except the last bucket, which contains > 99).
+NUM_AGE_BUCKETS = 21
+
 
 class IMDbFacialDataset(Dataset):
     """
@@ -40,7 +44,7 @@ class IMDbFacialDataset(Dataset):
         if self.transform:
             image = self.transform(image)
 
-        return image, self._get_age(file_path)
+        return image, self._get_age_bucket(file_path)
 
     def _get_file_path(self, idx):
         if idx >= self.__len__():
@@ -53,9 +57,12 @@ class IMDbFacialDataset(Dataset):
         name = os.listdir(subdir_path)[image_idx]
         return os.path.join(subdir_path, name)
 
-    def _get_age(self, file_path):
+    def _get_age_bucket(self, file_path):
         """
         Extracts the DOB and the year the photo was taken can calculates the age of the person.
+
+        Note that we use age buckets instead of the exact age.
+
         Args:
             file_path: Full path to the image file.
         Returns:
@@ -65,4 +72,4 @@ class IMDbFacialDataset(Dataset):
         parts = file_name.split('.')[0].split('_')
         dob = int(parts[2].split('-')[0])
         photo_token = int(parts[3])
-        return photo_token - dob
+        return (photo_token - dob) // 5

main.py
@@ -5,11 +5,10 @@ from torch.utils.data import sampler
 from torchvision import transforms, models
 
 from add_channel import AddChannel
-from dataset import IMDbFacialDataset
+from dataset import IMDbFacialDataset, NUM_AGE_BUCKETS
 from trainer import Trainer
 
 BATCH_SIZE = 500
-NUM_AGE_BUCKETS = 101
 DATA_LOADER_NUM_WORKERS = 5
 
 
@@ -33,22 +32,30 @@ def main():
     model_trainer = Trainer(
         model, loss_func, optimizer, device,
         loader_train, loader_val, loader_test,
-        num_epochs=20, print_every=25
+        num_epochs=20, print_every=50
     )
     model_trainer.train()
     model_trainer.test()
 
 
 def _split_data():
-    transform = transforms.Compose([
+    train_transform = transforms.Compose([
+        AddChannel(),
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(),
+        transforms.ToTensor(),
+    ])
+    val_transform = transforms.Compose([
         AddChannel(),
         transforms.ToPILImage(),
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
     ])
-    train_dataset = IMDbFacialDataset('imdb_crop', transform)
-    val_dataset = IMDbFacialDataset('imdb_crop', transform)
-    test_dataset = IMDbFacialDataset('imdb_crop', transform)
+    train_dataset = IMDbFacialDataset('imdb_crop', train_transform)
+    val_dataset = IMDbFacialDataset('imdb_crop', val_transform)
+    test_dataset = IMDbFacialDataset('imdb_crop', val_transform)
     # Do a rough 8:1:1 split between training set, validation set and test set.
     num_train = int(len(train_dataset) * 0.4)
     num_val = int(len(val_dataset) * 0.01)