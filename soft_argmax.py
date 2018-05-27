Fix softmax

soft_argmax.py
@@ -15,5 +15,6 @@ class SoftArgmaxLoss(_Loss):
 
     def forward(self, input, target):
         _assert_no_grad(target)
-        expected_class = (torch.nn.Softmax(input) * torch.arange(end=input.size())).sum()
+        num_classes = input.size()[1]
+        expected_class = (F.Softmax(input, dim=1) * torch.arange(end=num_classes)).sum(dim=1)
         return F.mse_loss(expected_class, target, size_average=self.size_average, reduce=self.reduce)