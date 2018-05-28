983d2cd3c483c5ee5482e568636fa7ae248d1863
Jiatu Liu <ljiatu@hotmail.com>
2018-05-28 18:15:04 +0000

    Fix normalizer

--- normalizer.py
+++ normalizer.py
@@ -14,28 +14,37 @@ def main():
     """
     transform = transforms.Compose([
         AddChannel(),
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
     ])
     dataset = IMDbFacialDataset('imdb_crop', transform)
-    loader = DataLoader(dataset, batch_size=400, num_workers=6)
+    loader = DataLoader(dataset, batch_size=2048, num_workers=6)
 
     running_mean = []
     running_std0 = []
     running_std1 = []
-    for i, (x, _) in enumerate(loader, 0):
+    for i, (x, _) in enumerate(loader):
         # shape (batch_size, 3 or 1, height, width)
+        print(f'Processing batch {i}')
         np_array = x.numpy()
 
         batch_mean = np.mean(np_array, axis=(0, 2, 3))
         batch_std0 = np.std(np_array, axis=(0, 2, 3))
         batch_std1 = np.std(np_array, axis=(0, 2, 3), ddof=1)
+        print('*' * 50)
+        print(f'Batch mean {batch_mean}')
+        print(f'Batch std ddof 0 {batch_std0}')
+        print(f'Batch std ddof 1 {batch_std1}')
+        print('*' * 50)
 
         running_mean.append(batch_mean)
         running_std0.append(batch_std0)
         running_std1.append(batch_std1)
 
-        running_mean = np.array(running_mean).mean(axis=0)
-        running_std0 = np.array(running_std0).mean(axis=0)
-        running_std1 = np.array(running_std1).mean(axis=0)
+    running_mean = np.array(running_mean).mean(axis=0)
+    running_std0 = np.array(running_std0).mean(axis=0)
+    running_std1 = np.array(running_std1).mean(axis=0)
 
     print('-' * 50)
     print(running_mean)
