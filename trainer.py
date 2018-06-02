d31bcaa48241e8a90a2f229e83b95f9816f68443
Jiatu Liu <jiatu@opendoor.com>
2018-06-02 14:17:15 -0700

    Try just the optimizer

--- trainer.py
+++ trainer.py
@@ -84,7 +84,7 @@ class Trainer:
             print(f'Val loss: {epoch_val_loss}, accuracy: {epoch_val_acc * 100}%')
             print('*' * 30)
 
-            self.scheduler.step(epoch_val_loss)
+            self.optimizer.step(epoch_val_loss)
             if epoch_val_acc > best_val_acc:
                 best_val_acc = epoch_val_acc
                 best_model_wts = copy.deepcopy(self.model.state_dict())
