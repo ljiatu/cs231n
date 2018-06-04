6d18d649d184bcc47b2a48b964c93694664df9c6
Jiatu Liu <jiatu@opendoor.com>
2018-06-04 00:22:26 -0700

    Fix chalearn dataset

--- chalearn_training_dataset.py
+++ chalearn_training_dataset.py
@@ -8,5 +8,5 @@ class ChaLearnTrainingDataset(ChaLearnDataset):
     This dataset returns the actual age instead of the file name.
     """
     def __getitem__(self, idx):
-        image, file_name = super().__getattribute__(idx)
+        image, file_name = super().__getitem__(idx)
         return image, int(round(float(self.labels[file_name])))
