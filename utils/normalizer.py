6c9bd0e479401ec603dae4113c749adbbe515e50
Jiatu Liu <jiatu@opendoor.com>
2018-06-04 23:56:00 -0700

    Update normalizer

--- utils/normalizer.py
+++ utils/normalizer.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 
 from constants import ETHNICITIES
-from datasets.imdb_wiki_ethnicity_dataset import IMDbWikiEthnicityDataset
+from datasets.chalearn_ethnicity_dataset import ChaLearnEthnicityDataset
 from utils.add_channel import AddChannel
 
 
@@ -27,7 +27,7 @@ def main():
     #     transform,
     # )
     for ethnicity in ETHNICITIES:
-        dataset = IMDbWikiEthnicityDataset(f'../imdb_wiki_ethnicity/{ethnicity}', transform)
+        dataset = ChaLearnEthnicityDataset(f'../ChaLearn/ethnicity/{ethnicity}', '../ChaLearn/ethnicity/gt.csv', transform)
         loader = DataLoader(dataset, batch_size=2048, num_workers=6)
 
         running_mean = []
